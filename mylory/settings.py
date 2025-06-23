d3755d165e101901f713cca6c49028662c21bc02
bawnekar99 <bawnekar031299@gmail.com>
2025-06-23 08:06:57 -0700

    OTP functionality improved with base URL changes

--- mylory/settings.py
+++ mylory/settings.py
@@ -60,9 +60,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-       'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+     
 }
 
 MIDDLEWARE = [
